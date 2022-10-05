77517f56d63cfa9d19753434cab6f66601a14f36
Saurabh2911 <54993976+Saurabh2911@users.noreply.github.com>
2022-10-05 23:13:18 +0530

    Add files via upload

---
+++ Chefandinteractive.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int n,r;
+	scanf("%d %d",&n,&r);
+	while(n--)
+	{
+		int R;
+		scanf("%d",&R);
+		if (R<r)
+		{
+			printf("Bad boi\n");
+		}
+		else
+		{
+			printf("Good boi\n");
+		}
+	}
+	return 0;
+}
